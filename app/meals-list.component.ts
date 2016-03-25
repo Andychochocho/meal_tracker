48b25a0527f95643644c212e79dcd9a4ab2c0de8
Andrew Cho <cho-andrew@hotmail.com>
2016-03-25 08:45:20 -0700

    add ability for user to add meals.

---
+++ app/meals-list.component.ts
@@ -0,0 +1,10 @@
+// import { Component, EventEmitter } from 'angular2/core';
+// import { MealComponent } from  './meal.component';
+// import { Meal } from './meal.model';
+//
+// @Component {
+//   selector: 'meals-list';
+//   inputs: ['mealsList'];
+//   outputs: ['onMealsSelect'],
+//   directives: [MealsComponent]
+// }
---
+++ app/new-meal.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './model.model';
+
+@Component ({
+  selector: 'new-meal';
+  outputs: ['onClickNewMeal'];
+  templateUrl: 'app/new-meal.component.html'
+})
+
+export class NewMealComponent {
+  public onClickNewMeal: EventEmitter<string>;
+  constructor() {
+    this.onClickNewMeal = new EventEmitter();
+  }
+  addMeal(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement) {
+    var mealList: string[] = [userName.value, userDetails.value, userCalories.value];
+    this.onClickNewMeal.emit(mealList);
+    userName.value = "";
+    userDetails.value = "";
+    userCalories.value = "";
+  }
+}
